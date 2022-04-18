5e8290e77cbf6a4d05499a2607d48b64ffc3faf6
Ayi <ayianjani774@gmail.com>
2022-04-18 08:34:39 +0700

    membuat kolom baru

--- database/migrations/2022_04_18_012744_create_articles_table.php
+++ database/migrations/2022_04_18_012744_create_articles_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->string('tittle');
+            $table->text('content');
+            $table->string('featured_image');
             $table->timestamps();
         });
     }
